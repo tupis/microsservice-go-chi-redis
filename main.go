d0e9c94a2f3d1acdadf48b74e9a9553a97affb42
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-12-31 12:01:43 -0300

    feat: add chi router

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("World"))
+}
