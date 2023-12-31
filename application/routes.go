b8ce8509a3d87389592df714e67ab7f030a3825d
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-12-31 17:06:15 -0300

    fix: add context

--- application/routes.go
+++ application/routes.go
@@ -3,12 +3,13 @@ package application
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/tupis/microsservice-go-chi-redis/repository/order"
 	"net/http"
 
 	"github.com/tupis/microsservice-go-chi-redis/handler"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 
 	router := chi.NewRouter()
 
@@ -18,17 +19,21 @@ func loadRoutes() *chi.Mux {
 		w.Write([]byte("Hello, World!"))
 	})
 
-	router.Route("/orders", loadOrdersRoutes)
+	router.Route("/orders", a.loadOrdersRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrdersRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App) loadOrdersRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo{
+			Client: a.rdb,
+		},
+	}
 
 	router.Get("/", orderHandler.List)
 	router.Post("/", orderHandler.Create)
 	router.Get("/{id}", orderHandler.GetByID)
-	router.Put("/{id}", orderHandler.UpdateById)
-	router.Delete("/{id}", orderHandler.DeleteById)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
