2e007a9b6367f34701e9cac76769c88b4279987b
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-12-31 14:06:28 -0300

    feat: add simple return

--- handler/orders.go
+++ handler/orders.go
@@ -5,12 +5,22 @@ import "net/http"
 type Order struct {
 }
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {}
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create order"))
+}
 
-func (o *Order) List(w http.ResponseWriter, r *http.Request) {}
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("List orders"))
+}
 
-func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {}
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get order by id"))
+}
 
-func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request) {}
+func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Update order by id"))
+}
 
-func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request) {}
+func (o *Order) DeleteById(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete order by id"))
+}
